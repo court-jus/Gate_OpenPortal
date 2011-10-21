28cc393e35dbe56efc1ffa738a2cb0746687df63
Ghislain LEVEQUE <ghislain.leveque@clarisys.fr>
2011-10-21 15:51:22 +0200

    Hide mouse cursor

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from direct.showbase.ShowBase import ShowBase
 from Gate.fps import FPS
 from Gate.player import Player
 from optparse import OptionParser
+from panda3d.core import WindowProperties
 
 def main():
     p = OptionParser()
@@ -12,6 +13,9 @@ def main():
     if a:
         levelname = a[0]
     base = ShowBase()
+    props = WindowProperties()
+    props.setCursorHidden(True)
+    base.win.requestProperties(props)
     fps = FPS(base, levelname)
     player = Player(base, fps)
     base.run()

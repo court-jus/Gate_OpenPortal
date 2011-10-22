2e5bfd86fa05cd45c5b2deec071a143cfb8ad867
Ghislain Leveque <ghislain.leveque@gmail.com>
2011-10-22 23:22:27 +0200

    Comment main code

--- main.py
+++ main.py
@@ -9,23 +9,34 @@ from optparse import OptionParser
 from panda3d.core import WindowProperties
 
 def main():
+    # Handles CLI arguments
     p = OptionParser()
     p.add_option('-m', '--nomusic', action="store_false", dest="music", default = True, help = u"Disable music")
     options, args = p.parse_args()
     levelname = 'level1'
     if args:
         levelname = args[0]
+
+    # Instantiate the ShowBase
     base = ShowBase()
+    
+    # Toggle events verbosity :
     #base.messenger.toggleVerbose()
+
+    # Hide mouse cursor
     props = WindowProperties()
     props.setCursorHidden(True)
     base.win.requestProperties(props)
+
+    # Now instantiate Gate's own stuff
     fps = FPS(base, levelname)
     osd = OSD(base)
     mplayer = MusicPlayer(base, osd)
     if options.music:
         mplayer.play_random_track()
     player = Player(base, fps)
+
+    # And run the ShowBase
     base.run()
 
 if __name__ == "__main__":

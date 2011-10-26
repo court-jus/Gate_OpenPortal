924f33023afe0bfe0ac8a9d9a97361011cbfae17
Ghislain LEVEQUE <ghislain.leveque@clarisys.fr>
2011-10-26 15:36:14 +0200

    Some examples

---
+++ tests/frame.py
@@ -0,0 +1,7 @@
+from direct.gui.DirectGui import *
+import direct.directbase.DirectStart
+ 
+myframe = DirectScrolledFrame(canvasSize = (-2,2,-2,2), frameSize = (-.5,.5,-.5,.5)) 
+myframe.setPos(0, 0, 0)
+ 
+run()
